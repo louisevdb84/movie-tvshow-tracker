98c20de7235f6e50c348e56c1da3ce9c9f7edf50
Louise van den Berg <louisevdb84@gmail.com>
2018-02-12 16:01:32 +0200

    Add a generic navbar and routing capabilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import App from './containers/App';
+import Navbar from './components/Navbar/Navbar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+    <Router>   
+    <div>    
+        <Navbar/>
+        <Route exact path="/" component= {App}/>
+        <Route exact path="/about" component= {Navbar}/>
+        <Route exact path="/Shop" component= {Navbar}/>
+    </div>
+    </Router>        
+    , document.getElementById('root'));
