06c8bb69914743a76b30f4184df5972a1873bf70
Louise van den Berg <louisevdb84@gmail.com>
2018-04-05 16:42:29 +0200

    Updated Movies

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -3,6 +3,7 @@ import MovieList from '../components/Shared/MovieList/MovieList';
 
  import Options from "../components/Movies/Options/Options";
 import Navbar from '../components/Shared/Navbar/Navbar';
+import Pagination from '../components/Movies/Pagination';
 
 
 class Movies extends Component {  
@@ -15,37 +16,95 @@ class Movies extends Component {
       genreList: [],     
       watchlistIds: [],
       watchedIds: [],
-      backupMovies: []
+      dislikeIds: [],
+      backupMovies: [],      
+      totalPages: 0,
+      page: 1
     }        
   }
 
 
-  componentDidMount() {      
-
+  componentDidMount() {     
+    this.getPages();     
     this.getWatchlist(); 
     this.getWatched(); 
+    this.getDislike();
+    this.getMovies();
+  }
 
+  getPages = () => {
+    fetch('https://safe-bayou-79396.herokuapp.com/countUpcoming')        
+    .then(response => response.json())
+      .then(count =>
+      {
+        this.setState({ totalPages: count })
+        console.log("Total Pages " + this.state.totalPages)          
+      })   
+      .catch(err => { console.log(err) });
+  }
+
+  prevPage = () => {   
+    
+    this.getPages();
+
+    if (this.state.page > 1 || this.state.page <= this.state.totalPages)
+    {
+      this.setState({ page: this.state.page - 1 });      
+      this.getMovies();
+    }  
+    else if (this.state.page > this.state.totalPages)
+    {
+      this.setState({ page: this.state.totalPages }); 
+      this.getMovies();
+    }
+      
+  }
+  nextPage = () => {  
+    
+    this.getPages();
+
+    if (this.state.page < this.state.totalPages)
+    {
+      this.setState({ page: this.state.page + 1 })
+      this.getMovies();
+    }        
+    else
+    {
+      this.setState({ page: this.state.totalPages })
+      this.getMovies();
+    }  
+  }
+
+  randomPage = () => {
+    this.getPages();
+    this.setState({ page:  Math.floor((Math.random() * this.state.totalPages) + 1) })
+    this.getMovies();
+  }
+
+
+  getMovies = () => {
     fetch('https://safe-bayou-79396.herokuapp.com/genres')
     .then(response => response.json())
       .then(genre => { 
         this.setState({genreList: genre.genres, genres: genre});
-        fetch('https://safe-bayou-79396.herokuapp.com/upcomingMovies', {
-              method: 'post',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({
-                page: 1
+            fetch('https://safe-bayou-79396.herokuapp.com/upcomingMovies', {
+                  method: 'post',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({
+                    page: this.state.page
+                })
             })
-        })
-        .then(response => response.json())
-        .then(mov => {
-          this.setState({
-            upcomingMovies: this.addingGenres(mov, genre),                  
-            backupMovies: this.addingGenres(mov, genre),                  
-          })
-        });             
-           
+            .then(response => response.json())
+              .then(mov => {
+              if(mov.length>0)
+                this.setState({
+                  upcomingMovies: this.addingGenres(mov, genre),                  
+                  backupMovies: this.addingGenres(mov, genre),                  
+                })
+            })          
+            .catch(err => { console.log(err) });
       })  
-      
+      .catch(err => { console.log(err) });
   }
 
   addingGenres = (mov, genre) => {
@@ -153,9 +212,24 @@ class Movies extends Component {
       })
   }  
 
+  getDislike = () => {
+    fetch('https://safe-bayou-79396.herokuapp.com/dislike', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: sessionStorage.getItem("user")
+      })
+    })
+      .then(response => response.json())
+      .then(list => {
+        if (list) {
+          this.setState({ dislikeIds: list });          
+        }
+      })
+  }  
    
   render() {         
-    const { baseURL, upcomingMovies, genreList,watchlistIds,  watchedIds} = this.state;
+    const { baseURL, upcomingMovies, genreList, watchlistIds, watchedIds, dislikeIds } = this.state;
     
     return (        
       
@@ -168,11 +242,15 @@ class Movies extends Component {
             onGenreChange={this.onGenreChange} />  
           <div className="tc"> 
             
-            <h1 className = "moviesheading">Upcoming Movies</h1>
+          <h1 className="moviesheading">Upcoming Movies</h1>
+
           {!sessionStorage.getItem("user") ? <div style={{ "background": "red", "color": "white"}}>Your are not signed in</div> : <div></div>}
           {upcomingMovies.length > 0 ?
-            
-            <MovieList movies={upcomingMovies} baseURL={baseURL} opt="Movies" watchlistIds={watchlistIds} watchedIds={watchedIds}/>              
+            <div>
+              <Pagination totalPages={this.state.totalPages} page={this.state.page} prevPage={this.prevPage} nextPage={this.nextPage} randomPage={this.randomPage}></Pagination>
+                <MovieList movies={upcomingMovies} baseURL={baseURL} opt="Movies" watchlistIds={watchlistIds} watchedIds={watchedIds} dislikeIds={dislikeIds}/>              
+              <Pagination totalPages={this.state.totalPages} page={this.state.page} prevPage={this.prevPage} nextPage={this.nextPage} randomPage={this.randomPage}></Pagination>
+              </div>  
               : <p>Loading</p>
               }
           </div>   
